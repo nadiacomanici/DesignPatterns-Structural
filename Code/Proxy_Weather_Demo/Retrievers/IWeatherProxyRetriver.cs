f71163ec939c6b00ee403532ba31d41010809881
nadiacomanici <nadi_comanici@yahoo.com>
2020-03-24 10:22:08 +0200

    Proxy examples & slides during .Net Community Meetup

---
+++ Code/Proxy_Weather_Demo/Retrievers/IWeatherProxyRetriver.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Proxy_Weather_Demo.Retrievers
+{
+    public interface IWeatherProxyRetriver
+    {
+        double? GetPredictionForDate(string cityId, DateTime referenceDate);
+    }
+}
---
+++ Code/Proxy_Weather_Demo/Retrievers/WeatherProxyRetriver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Proxy_Weather_Demo.Helpers;
+using Proxy_Weather_Demo.Models;
+
+namespace Proxy_Weather_Demo.Retrievers
+{
+    public class WeatherProxyRetriver : IWeatherProxyRetriver
+    {
+        private WeatherApiRetriever _realWeatherApiRetriever;
+        private readonly string _apiKey;
+
+        private List<Prediction> _cachedPredictions;
+        private DateTime _cachedExpirationTimestamp;
+
+        private WeatherApiRetriever WeatherApiRetriever
+        {
+            get
+            {
+                if (_realWeatherApiRetriever == null)
+                {
+                    _realWeatherApiRetriever = new WeatherApiRetriever(_apiKey);
+                }
+                return _realWeatherApiRetriever;
+            }
+        }
+
+        public WeatherProxyRetriver()
+        {
+            _apiKey = ConfigurationManager.AppSettings.Get("weatherApiKey");
+        }
+
+        private bool IsCachedExpired
+        {
+            get { return _cachedPredictions == null || _cachedPredictions.Count == 0 || _cachedExpirationTimestamp < DateTime.Now; }
+        }
+
+        public double? GetPredictionForDate(string cityId, DateTime referenceDate)
+        {
+            if (IsCachedExpired)
+            {
+                _cachedPredictions = WeatherApiRetriever.GetPredictionsForCity(cityId);
+                _cachedExpirationTimestamp = DateTime.Now.AddMinutes(1);
+            }
+            return _cachedPredictions.GetPredictionByDate(referenceDate)?.TemperatureInCelsius;
+        }
+    }
+}
